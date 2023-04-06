bf17ddbb4b3aa884ffa3fefa9520c4f08807c808
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-06 18:45:40 +0300

    added folders

---
+++ SqlLauncherAPP/Services/ISqlFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLauncherAPP.Services
+{
+    public interface ISqlFileService
+    {
+        public void RunSqlFileScripts();
+
+    }
+}
---
+++ SqlLauncherAPP/Services/SqlFileService.cs
@@ -0,0 +1,23 @@
+﻿using SqlLauncherAPP.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLauncherAPP.Services
+{
+    internal class SqlFileService:ISqlFileService
+    {
+        private readonly ISqlFileRepository n_Repository;
+        public SqlFileService( ISqlFileRepository repository)
+        { 
+            n_Repository = repository?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public void RunSqlFileScripts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
